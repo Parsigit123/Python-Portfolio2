c9e0f2ac1521a16a9c9d3cc41a7a1f75c553b426
parsi <parsova.khayatan@gmail.com>
2023-03-03 14:45:43 +0100

    Rended the titles.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
+import pandas
 
 with col1:
     st.image("images/photo.png", width=400)
@@ -20,3 +21,15 @@ content2 = """
 Below you can find some of the apps I've built in Python. Feel free to contact me!
 """
 st.write(content2)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+col3, col4 = st.columns(2)
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
